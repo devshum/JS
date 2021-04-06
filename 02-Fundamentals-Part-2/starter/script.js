d52f245ba2f20efee133528d66d33f6c6cfe7283
devshum <57689318+devshum@users.noreply.github.com>
2021-04-06 11:58:13 +0300

    functions boilerplate

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,13 +1,1 @@
-'use strict';
-
-let hasDriversLicense = false;
-const passTest = true;
-
-if(passTest) hasDriverLicense = true;
-
-if(hasDriversLicense) console.log('I can drive :D');
-
-// const interface = 'Audio';
-// const private = 534;
-
 
