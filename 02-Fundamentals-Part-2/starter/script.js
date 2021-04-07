495d53093d672260d35d6b6a3d3be65558b54c26
devshum <57689318+devshum@users.noreply.github.com>
2021-04-07 17:26:24 +0300

    introduction to objects

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,2 +1,17 @@
 'use strict';
 
+const jonasArray = [
+    'Jonas',
+    'Schmedtmann',
+    2037 - 1991,
+    'teacher',
+    ['Michael', 'Peter', 'Steven']
+];
+
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtmann',
+    age: 2037 - 191,
+    job: 'teacher',
+    friends: ['Michael', 'Peter', 'Steven']
+};
