403c430f72c94c3857fbb738813167a8ba755fe5
devshum <57689318+devshum@users.noreply.github.com>
2021-04-07 16:16:23 +0300

    introduction to objects

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,26 +1,2 @@
 'use strict';
 
-const bills = [125, 555, 44];
-
-const calcTip = bills => {
-  const billsArr = bills;
-
-  const tipsArr = bills.map(bill => {
-    switch (true) {
-      case bill >= 50 && bill <= 300:
-        return Math.ceil(bill * .15)
-      default:
-        return Math.ceil(bill * .2);
-    };
-  });
-
-  const billsAndTipsArr = billsArr.map((bill, i) => bill + tipsArr[i]);
-
-  return {
-    billsPlusTips: billsAndTipsArr, // [144, 666, 53]
-    bills: billsArr, // [125, 555, 44]
-    tips: tipsArr  // [19, 111, 9]
-  };
-};
-
-const result = calcTip(bills);
