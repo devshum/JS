65bac4464c2fffc0d332e98462a80e60f8b6de1d
devshum <57689318+devshum@users.noreply.github.com>
2021-04-07 13:34:57 +0300

    basic arrays methods

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,2 +1,30 @@
 'use strict';
 
+// Add elements
+const friends = ['Michael', 'Steven', 'Peter'];
+
+const newLength = friends.push('Jay');
+
+console.log(friends);
+console.log(newLength);
+
+friends.unshift('John');
+
+console.log(friends);
+
+// Remove elements
+friends.pop();
+const popped = friends.pop();
+console.log(friends);
+console.log(popped);
+
+friends.shift();
+console.log(friends);
+
+console.log(friends.indexOf('Steven'));
+
+console.log(friends.includes('Steven'));
+
+if (friends.includes('Steven')) {
+  console.log('You have a friend called Peter');
+} 
