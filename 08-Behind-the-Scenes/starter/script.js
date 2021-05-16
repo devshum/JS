3350433240681f3c4cce40b71a3f10134c57fa1c
devshum <57689318+devshum@users.noreply.github.com>
2021-05-16 10:14:35 +0300

    arrow functions vs regular functions

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,34 +1,47 @@
 'use strict';
 
-// console.log(this);
-
-// const calcAge = function() {
-//     console.log(this);
-// };
-
-// calcAge();
+// var firstName = 'Matilda';
 
 const jonas = {
+    firstName: 'Jonas',
     year: 1991,
     calcAge: function() {
         console.log(this);
+        console.log(2037 - this.year);
+
+        // const self = this;
+        // const isMillenial = function() {
+        //     // console.log(this.year >= 1981 & this.year <= 1996)
+
+        //     console.log(self.year >= 1981 & self.year <= 1996)
+        // };
+
+        const isMillenial = () => {
+            console.log(this.year >= 1981 & this.year <= 1996)
+        }
+
+        isMillenial();
     },
 
-    calcAgeArrow: () => {
-        console.log(this)
-    }
+    greet: () => console.log(`Hey ${this.firstName}`)
 };
 
+jonas.greet();
 jonas.calcAge();
-jonas.calcAgeArrow();
 
-const matilda = {
-    year: 2017,
-};
+// arguments keyword
+const addExpr = function(a, b) {
+    console.log(arguments);
+    return a + b;
+}
 
-matilda.calcAge = jonas.calcAge;
-matilda.calcAge();
+addExpr(2, 5);
+addExpr(2, 5, 8, 12);
 
-const f = jonas.calcAge;
 
-f()
+const addArrow = (a, b) => {
+    console.log(arguments);
+    return a + b;
+}
+
+addArrow(2, 5);
