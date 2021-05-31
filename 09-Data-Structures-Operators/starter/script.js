9672835a38a0b2cf55e33742595066b6e5338fc1
devshum <57689318+devshum@users.noreply.github.com>
2021-05-31 17:07:31 +0300

    working with arrays part 3

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -6,14 +6,16 @@ const [firstName, lastName] = 'Vlad Shum'.split(' ');
 
 const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
 
-// Solution 1
 const capitalizeName = name => {
     const splitName = name.split(' ');
     
     const capitalizedName = [];
     
     for(const namePart of splitName) {
-        capitalizedName.push(namePart[0].toUpperCase() + namePart.slice(1))
+        // solution 1
+        // capitalizedName.push(namePart[0].toUpperCase() + namePart.slice(1))
+        // solution 2
+        capitalizedName.push(namePart.replace(namePart[0], namePart[0].toUpperCase()));
               
     }
 
@@ -22,6 +24,35 @@ const capitalizeName = name => {
 
 console.log(capitalizeName('jessica ann smith davis'));
 console.log(capitalizeName('vlad shum'));
+
+// Padding
+const message = 'Go to gate 23!'
+console.log(message.padStart(25, '+').padEnd(35, '+'));
+console.log('Vlad'.padStart(25, '+').padEnd(35, '+'));
+
+const maskCreditCard = number => {
+    const str = number + '';
+    const last = str.slice(-4);
+
+    return last.padStart(str.length, '*');
+};
+
+console.log(maskCreditCard(4335566743356677));
+console.log(maskCreditCard('3556674335667755'));
+
+// Repeat
+const message2 = 'Bad weather... All Departures Delayed...';
+
+console.log(message2.repeat(5));
+
+const planesInLine = n => {
+    console.log(`There are ${n} planes in line ${'✈'.repeat(n)}`);
+};
+
+planesInLine(3);
+planesInLine(9);
+planesInLine(2);
+
 
 
 
