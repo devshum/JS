9550f9a71c1eb4c6e2b5b2b36db3ef451415c3f5
devshum <57689318+devshum@users.noreply.github.com>
2021-06-01 11:17:40 +0300

    boilerplatee

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,13 +1,2 @@
 'use strict';
 
-const greet = greeting => {
-    return name => {
-        console.log(`${greeting} ${name}`);
-    }
-}
-
-greet('Hello')('Vlad');
-
-const greetLine = greeting => name => console.log(`${greeting} ${name}`);
-
-greetLine('Hello')('Mr')
