446a908b10afff4d0f3bd7fd373c42933763297d
devshum <57689318+devshum@users.noreply.github.com>
2021-06-03 17:22:22 +0300

    IIFE

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,71 +1,19 @@
 'use strict';
 
-///////////////////////////////////////
-// Coding Challenge #1
+(function() {
+  console.log('This will never run again');
 
-/* 
-Let's build a simple poll app!
+  const isPrivate = 23;
+})();
 
-A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter object below.
+// console.log(isPrivate);
 
-Here are your tasks:
+(() => console.log('This will ALSO never run again'))();
 
-1. Create a method called 'registerNewAnswer' on the 'poll' object. The method does 2 things:
-  1.1. Display a prompt window for the user to input the number of the selected option. The prompt should look like this:
-        What is your favourite programming language?
-        0: JavaScript
-        1: Python
-        2: Rust
-        3: C++
-        (Write option number)
-  
-  1.2. Based on the input number, update the answers array. For example, if the option is 3, increase the value AT POSITION 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g answer 52 wouldn't make sense, right?)
-2. Call this method whenever the user clicks the "Answer poll" button.
-3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1". 
-4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
-
-HINT: Use many of the tools you learned about in this and the last section 😉
-
-BONUS: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' option. Do NOT put the arrays in the poll object! So what shoud the this keyword look like in this situation?
-
-BONUS TEST DATA 1: [5, 2, 3]
-BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
-
-GOOD LUCK 😀
-*/
-
-const poll = {
-  question: 'What is your favourite programming language?',
-  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
-  answers: new Array(4).fill(0),
-
-  registerNewAnswer() {
-    const answer = Number(prompt(`
-        ${this.question}\n${this.options.join('\n')}\n(Write option number)
-    `));
-
-    answer < this.answers.length && this.answers[answer]++;
-
-    this.displayResults('');
-  },
-
-  displayResults(type) {
-    switch (true) {
-      case typeof type === 'string':
-        console.log(`Poll results are: ${this.answers.join(' ')}`);
-        break;
-      default:
-        console.log(this.answers);
-        break;
-    }
-  }
+{
+  const isPrivate = 23;
+  var notPrivate = 46;
 }
 
-document.querySelector('.poll').addEventListener('click', poll.registerNewAnswer.bind(poll));
-
-var answers = [1, 5, 3, 9, 6, 1];
-
-const displayOtherAnswers = poll.displayResults.call(this, '', ...answers);
-
-
-
+// console.log(isPrivate);
+console.log(notPrivate);
