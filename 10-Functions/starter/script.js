359d306574a4f549c34e58d33067e66e854e25d9
devshum <57689318+devshum@users.noreply.github.com>
2021-06-03 17:52:52 +0300

    boilerplate

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,16 +1,2 @@
 'use strict';
 
-const secureBooking = () => {
-  let passengerCount = 0;
-
-  return () => {
-    passengerCount++;
-    console.log(`${passengerCount} passengers`);
-  };
-};
-
-const booker = secureBooking();
-
-booker();
-booker();
-booker();
